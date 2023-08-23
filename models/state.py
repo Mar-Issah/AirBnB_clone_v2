c718e2b316c1e97e434d5709dac9d47f2899ed2c
Mar-Issah <masy370@gmail.com>
2023-08-23 12:49:01 -0400

    update file models/__init__.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import models
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if models.storage_type == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='delete')
+    else:
+        name = ""
+
+    if models.storage_type != 'db':
+        @property
+        def cities(self):
+            city_list = []
+            all_cities = models.storage.all(City).values()
+            for city in all_cities:
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
